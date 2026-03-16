e35dcbe9b98394931764cdbc2feb58a158299e0e
Aryan Vasudevan <vasudevan.aryan@gmail.com>
2026-03-15 20:46:11 -0400

    Landing page UI, proper scaling

--- frontend/src/Landing.jsx
+++ frontend/src/Landing.jsx
@@ -1,9 +1,16 @@
 import './Landing.css'
 import landingBg from './assets/backgrounds/landing.png'
+import startAppBtn from './assets/buttons/start-application.png'
 
 function Landing() {
   return (
-    <div className="landing" style={{ backgroundImage: `url(${landingBg})` }}>
+    <div className="landing">
+      <div className="landing-content">
+        <img src={landingBg} alt="" className="landing-bg" />
+        <button className="start-application-btn">
+          <img src={startAppBtn} alt="Start Application" />
+        </button>
+      </div>
     </div>
   )
 }
