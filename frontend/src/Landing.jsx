0378d815277306f5dea6aec81955f69e5d3b7fe5
Aryan Vasudevan <vasudevan.aryan@gmail.com>
2026-03-15 20:38:16 -0400

    Landing background, resizing fix

---
+++ frontend/src/Landing.jsx
@@ -0,0 +1,11 @@
+import './Landing.css'
+import landingBg from './assets/backgrounds/landing.png'
+
+function Landing() {
+  return (
+    <div className="landing" style={{ backgroundImage: `url(${landingBg})` }}>
+    </div>
+  )
+}
+
+export default Landing
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import Landing from './Landing.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Landing />
   </StrictMode>,
 )
