fb0220852f7f14f9e65f6153f9ad481d0a88f6ea
ylabidi11 <youssef.labidi35@gmail.com>
2026-05-07 22:44:08 -0400

    merge refactor/frontend-structure into feat/reviewer-screens - keep reviewer routes approach

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,42 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./pages/auth/Login";
-import Signup from "./pages/auth/Signup";
-import ForgotPassword from "./pages/auth/ForgotPassword";
-import ResetPassword from "./pages/auth/ResetPassword";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-import Landing from "./pages/hacker/Landing";
-import Dashboard from "./pages/hacker/Dashboard";
-import Info from "./pages/hacker/Info";
-import Education from "./pages/hacker/Education";
-import Experience from "./pages/hacker/Experience";
-import Teammates from "./pages/hacker/Teammates";
-import Submission from "./pages/hacker/Submission";
+import routes from "./routes/routes.jsx";
 
 import "./index.css";
 
+const router = createBrowserRouter(routes);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        {/* Auth */}
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-
-        {/* Hacker */}
-        <Route path="/landing" element={<Landing />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/info" element={<Info />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/teammates" element={<Teammates />} />
-        <Route path="/submission/:id" element={<Submission />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 );
